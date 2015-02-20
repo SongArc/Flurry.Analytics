4916e7f8f09c0d2000dbd580e0323e3c1878494e
Nezz <bloodsource@vipmail.hu>
2015-02-20 02:52:37 +0100

    Android compatibility with Windows API

--- FlurryAnalytics/binding/Flurry.Analytics.Android/Additions/FlurryAgent.cs
+++ FlurryAnalytics/binding/Flurry.Analytics.Android/Additions/FlurryAgent.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using Java.Lang;
+using Exception = System.Exception;
 
 namespace Flurry.Analytics
 {
@@ -8,6 +10,37 @@ namespace Flurry.Analytics
 		{
 			FlurryAgent.SetGender ((sbyte)gender);
 		}
+
+        public static void LogEvent(string eventName, List<Parameter> parameters)
+        {
+            FlurryAgent.LogEvent(eventName, ParametersToDictionary(parameters));
+        }
+
+        public static void LogEvent(string eventName, List<Parameter> parameters, bool timed)
+        {
+            FlurryAgent.LogEvent(eventName, ParametersToDictionary(parameters), timed);
+        }
+
+        public static void LogError(string message, Exception exception)
+        {
+            FlurryAgent.LogError(exception.Message, message, new Throwable(exception.Message));
+        }
+
+        public static void EndTimedEvent(string eventName, List<Parameter> parameters)
+        {
+            FlurryAgent.EndTimedEvent(eventName, ParametersToDictionary(parameters));
+        }
+
+        private static IDictionary<string, string> ParametersToDictionary(List<Parameter> parameters)
+        {
+            var dict = new Dictionary<string, string>();
+            foreach (var parameter in parameters)
+            {
+                dict[parameter.Name] = parameter.Value;
+            }
+
+            return dict;
+        }
 	}
 
 	public enum Gender
@@ -16,5 +49,17 @@ namespace Flurry.Analytics
 		Female = 0,
 		Unknown = -1
 	}
+
+    public class Parameter
+    {
+        public string Name { get; private set; }
+        public string Value { get; private set; }
+
+        public Parameter(string name, string value)
+        {
+            this.Name = name;
+            this.Value = value;
+        }
+    }
 }
 
